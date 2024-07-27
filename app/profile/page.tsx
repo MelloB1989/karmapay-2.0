61f3ae56c8d9168f7d590d16a5646fc43d3d2050
AdityaKulkarniXD <beastwolftheleader@gmail.com>
2024-07-27 22:29:06 +0530

    created profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+import Nav from '@/components/landing/nav'
+import React from 'react'
+import Profile from './Profile'
+
+export default function page() {
+  return (
+    <div><Profile/>
+    </div>
+    
+    
+
+  )
+}
