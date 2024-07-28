c5bd3afa9cd81ca863ae0c69c57dfae8f3fcf580
Kaarthik069 <22eg107b63@anurag.edu.in>
2024-07-28 03:53:11 +0000

    intergration for register api

--- config.ts
+++ config.ts
@@ -1,3 +1,6 @@
 export const config = {
-    logo_url: 'https://noobsverse-internal.s3.ap-south-1.amazonaws.com/karmapay-removebg-preview.png'
+    logo_url: 'https://noobsverse-internal.s3.ap-south-1.amazonaws.com/karmapay-removebg-preview.png',
+    api_url:  'http://localhost:9080',
+    api_v:     'v1'
 }
+
